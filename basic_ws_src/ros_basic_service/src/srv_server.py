25be09489ee078bf5211aaa6c7697668e039a7ff
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-08-26 14:03:44 +0900

    Add files via upload

---
+++ basic_ws_src/ros_basic_service/src/srv_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rospy
+
+from ros_basic_service.srv import WordCount,WordCountResponse
+
+
+def count_words(request):
+    return WordCountResponse(len(request.words.split()))
+
+
+rospy.init_node('srv_server')
+
+#TODO
+service = rospy.Service('word_count', WordCount, count_words)
+
+rospy.spin()
+
---
+++ basic_ws_src/ros_basic_test/src/rostopic_pub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+
+#TODO
+rospy.init_node('rostopic_pub')
+pub = rospy.Publisher('counter', Int32, queue_size=10)
+
+rate = rospy.Rate(2)
+count = 0
+
+while not rospy.is_shutdown():
+    pub.publish(count)
+    count += 1
+    rate.sleep()
+
